69a5e6b588da993c43150a4fdc5981ad7199c041
majjikishore007 <majjikishore031@gmail.com>
2021-08-10 20:33:33 +0530

    updated

--- frontend/day19and21/src/components/Element.js
+++ frontend/day19and21/src/components/Element.js
@@ -1,44 +1,52 @@
 import React, { useState } from "react";
 
 const Element = (props) => {
-  const [isEditing, setIsEditing] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
   const { deleteItem, updateItem, item, index } = props;
   return (
     <div>
-      {isEditing ? (
-        <input
-          type='text'
-          name='name'
-          id='name'
-          required
-          value={item.name}
-          onChange={(e) => updateItem(index, { title: e.target.value })}
-        />
-      ) : (
-        <h5 className='card-title'>{item.title}</h5>
-      )}
-      {isEditing ? (
-        <input
-          type='number'
-          name='calorie'
-          id='calorie'
-          required
-          value={item.calorie}
-          onChange={(e) => updateItem(index, { calorie: e.target.value })}
-        />
-      ) : (
-        <p className="='card-text'">
-          {" "}
-          You have consumed {item.calorie} calories
-        </p>
-      )}
+      {console.log("EDITING ", isEditing)}
+      {console.log(item)}
+      <div className='card-body'>
+        {isEditing ? (
+          <input
+            type='text'
+            name='title'
+            id='title'
+            required
+            value={item.name}
+            onChange={(e) =>
+              updateItem(index, { ...item, name: e.target.value })
+            }
+          />
+        ) : (
+          <h5 className='card-title'>{item.name}</h5>
+        )}
+        {isEditing ? (
+          <input
+            type='number'
+            name='calorie'
+            id='calorie'
+            required
+            value={item.calorie}
+            onChange={(e) =>
+              updateItem(index, { ...item, calorie: e.target.value })
+            }
+          />
+        ) : (
+          <p className="='card-text'">
+            {" "}
+            You have consumed {item.calorie} calories
+          </p>
+        )}
 
-      <button className='' onClick={() => deleteItem(index)}>
-        Delete
-      </button>
-      <button className='' onClick={() => setIsEditing(!isEditing)}>
-        {isEditing ? "Done" : "Edit"}
-      </button>
+        <button className='' onClick={() => deleteItem(index)}>
+          Delete
+        </button>
+        <button className='' onClick={() => setIsEditing(!isEditing)}>
+          {isEditing ? "Done" : "Edit"}
+        </button>
+      </div>
     </div>
   );
 };
